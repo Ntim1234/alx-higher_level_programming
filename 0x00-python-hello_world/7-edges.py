482213b26e068c413db5fa0ea7521ec41ba92714
Ntim1234 <macdumanlegacy@gmail.com>
2023-03-08 14:25:55 -0800

    task update

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+bb82b2861b4379a9b67a
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle-word = word[1:-1]
+print(f"First 3 letters:{word_first_3}")
+print(f"Last 2 letters:{word_last_2}")
+print("Middle word: {}".format(middle_word))
